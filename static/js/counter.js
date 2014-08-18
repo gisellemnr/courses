02279c94fa8b8fce7858a984d2e21f4562041831
Kanbour Sama <samak@cmu.edu>
2014-08-18 21:58:38 +0300

    color to counter

--- static/js/counter.js
+++ static/js/counter.js
@@ -2,10 +2,11 @@ function Counter() {
 	var COUNTERS = [];
 	var TEXTS = [];
 	var r = Raphael("counter", 45, 580),
-		R = 6, // radius of circle 
-		xi = 10, // x position
-		total = 6, // number of stats
-		marksAttr = { stroke: "lightgrey", fill: "none", "stroke-width": 4 },
+		R = 6, 		// radius of circle 
+		xi = 10, 	// x position
+		sw = 5, 	// stroke-width
+		total = 4, 	// number of stats
+		marksAttr = { stroke: "lightgrey", fill: "none", "stroke-width": sw },
 		labelAttr = { stroke: "none", fill: "grey", "font-size": "12px" };
 	
 	addObj();
@@ -16,14 +17,15 @@ function Counter() {
 			a = (90 - alpha) * Math.PI / 180,
 			x = xi + R * Math.cos(a),
 			y = yi - R * Math.sin(a),
-			color = Raphael.hsb( (total - value) / total, 1, .75),
+			color = Raphael.hsb(0.7 - value * 0.17, 1, .8),
 			path;
+		// 0.6, 0.5, 0.3, 0.2, 0
 		if (value == total) {
 			path = [["M", xi, yi - R], ["A", R, R, 0, 1, 1, xi - 0.01, yi - R]];
 		} else {
 			path = [["M", xi, yi - R], ["A", R, R, 0, + (alpha > 180), 1, x, y]];			
 		}
-		return { path: path, stroke: color, opacity: .8, "stroke-width": 4 };
+		return { path: path, stroke: color, opacity: .8, "stroke-width": sw };
 	}
 
 	function addObj(){
@@ -39,16 +41,14 @@ function Counter() {
 		var value;
 		if (v < 1) {
 			value = 0;
-		} else if (v < 21) {
-			value = 1;
 		} else if (v < 42) {
-			value = 2;
+			value = 1;
 		} else if (v < 52) {
-			value = 3;
+			value = 2;
 		} else if (v < 62) {
-			value = 4;
+			value = 3;
 		} else {
-			value = 5;
+			value = 4;
 		}
 		var id = 7 - i;
 		var y = (id + 1) * 75 - 50;
