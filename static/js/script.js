4a83b8d4ddc21d086407c92a9101197e20b0a7c6
Kanbour Sama <samak@cmu.edu>
2014-08-24 00:33:20 +0300

    .

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
 	setUp();
 
-	console.log('hi');
+	try {
+		$.get('data.php');
+	} catch (e) {
+		console.log('404');		
+	}
 
-	$.get('data.php').done(function() { 
-        $.dbGET('getUsername', {}, function(r) {
-			if (r) {
-				$('#log div').html("LOGOUT " + r.toUpperCase());
-				$('a#log').css('width', '200px');
-			}
-		});
-    }).fail(function() { 
-        console.log(false);
-    });
+	// $.dbGET('getUsername', {}, function(r) {
+	// 	if (r) {
+	// 		$('#log div').html("LOGOUT " + r.toUpperCase());
+	// 		$('a#log').css('width', '200px');
+	// 	}
+	// });
 
 	Tabletop.init({
 		key: "0AhtG6Yl2-hiRdE9KVHEtSkxscnoxTExua3dyNXJZUXc",
