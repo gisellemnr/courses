4ec12c869b967ad104b5d7ea4655ab384e81330b
Kanbour Sama <samak@cmu.edu>
2014-08-24 14:44:09 +0300

    legend

--- static/js/counter.js
+++ static/js/counter.js
@@ -8,9 +8,9 @@ function Counter() {
 		parts = 5, 	// number of stats
 		labelAttr = { stroke: "none", fill: "grey", "font-size": "12px" },
 		circleAttr = { fill: "#555", stroke: "none" };
-	addObj();
 	
-	Raphael.colorwheel("counter-legend", 250);
+	addObj();
+	legend("counter-legend", 250);
 
 	function addObj() {
 		var p = 80,
@@ -50,6 +50,7 @@ function Counter() {
 						TEXTS[k].tooltip.hide();
 	            	}
 	            	var t = Math.round((e.y + 50) / 75) - 1;
+	            	console.log(t);
 	            	TEXTS[t].tooltitle.show();
 					TEXTS[t].tooltip.show();
 	            }, function (e) {
@@ -63,7 +64,6 @@ function Counter() {
                 stroke: "none"
             });
             NEEDLES.push(needle);
-
 		}
 		$("#counter").click(function () {
 			$(".target").hide();
@@ -74,6 +74,59 @@ function Counter() {
 			}
 		});
 	}
+
+	function legend(el, size) {
+		var pi = Math.PI,
+        	labelAttr = { stroke: "none", fill: "grey", "font-size": "12px" },
+		    cursorAttr = { fill: "#222", stroke: "none", cursor: "move" },
+            parts = 5,
+            w3 = 3.5 * size / 200,
+            size20 = size / 10,
+            size2 = size / 2,
+            size4 = size / 2.4,
+            segments = parts * 15,
+            padding = size20 * 1.8,
+            t = this;
+		var r = Raphael(el, size, size * 2);
+		t.size2 = size2;
+		var a = pi / 2 - pi * 2 / segments * 1.3 / 2,
+		    R = size2 - padding,
+		    R2 = R - size20 * 2,
+		    path = ["M", size2, padding, "A", R, R, 0, 0, 1, R * Math.cos(a) + R + padding, R - R * Math.sin(a) + padding, "L", R2 * Math.cos(a) + R + padding, R - R2 * Math.sin(a) + padding, "A", R2, R2, 0, 0, 0, size2, padding + size20 * 2, "z"].join();
+		for (var i = 0; i < segments; i++) {
+		    var fill = i * (150 / segments) / 255;
+		    if (i > segments / 2) {
+		        fill = (segments - i) * (150 / segments) / 255;
+		    }
+		    r.path(path).attr({
+		        stroke: "none",
+		        fill: Raphael.hsb(fill, .9, .78),
+		        transform: "r" + [(180 / segments) * i - 90, size2, size2]
+		    });
+		}
+		t.cur = r.path(["M", size2, size4, "L", size2 + w3 * 3, size4 - w3 * 4, "L", size2, size4 - w3 * 12, "L", size2 - w3 * 3, size4 - w3 * 4]).attr(cursorAttr);
+		t.cur.drag(function (dx, dy, x, y) {
+		    setH(x - t.size2 - 10, y - t.size2 - 150, t);
+		});
+		r.text(size2, 5, "UNIT COUNTER").attr(labelAttr).attr({"font-size": "14px", "font-weight": "bold"});
+		r.text(size2, size20 - 5, "36").attr(labelAttr).attr({transform: "r" + [-270 / parts,   size2, size2] });
+		r.text(size2, size20 - 5, "46").attr(labelAttr).attr({transform: "r" + [-90 / parts,    size2, size2] });
+		r.text(size2, size20 - 5, "52").attr(labelAttr).attr({transform: "r" + [90 / parts,     size2, size2] });
+		r.text(size2, size20 - 5, "64").attr(labelAttr).attr({transform: "r" + [270 / parts,    size2, size2] });
+		t.leg = r.text(size2, 80, "Drag the needle").attr(labelAttr);
+		r.text(10, 100, " 0-35: You need at least 36 units\n36-40: You may not be able to drop a course\n41-48: Healthy load\n49-50: You are piling too much on your plate\n51-63: requires permission if QPA < 3.0").attr(labelAttr).attr({"text-anchor": "start"});
+	}
+
+	function setH(x, y, t) {
+		var labels = ["Underload", "Risky", "Healthy", "Overload", "Permissions"];
+        var d = Raphael.angle(x, y, 0, 0);
+        if (d < 90) { d = 360 }
+        else if (d < 180) { d = 180 };
+        var i = Math.floor((d - 180) / 36);
+        if (i > 4) { i = 4 };
+        t.cur.attr({transform: "r" + [d + 90, this.size2, this.size2]});
+        t.leg.attr({text: labels[i], y: 155});
+    }
 
 	this.updateVal = function (i, v) {
 		var value;
