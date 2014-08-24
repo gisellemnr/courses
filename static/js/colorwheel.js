f0d6fad006699d8e606d7fc45b01b304ccf073b9
Kanbour Sama <samak@cmu.edu>
2014-08-24 19:46:36 +0300

    color wheel edited

---
+++ static/js/colorwheel.js
@@ -0,0 +1,61 @@
+(function (Raphael) {
+    Raphael.colorwheel = function (size) {
+        return new ColorWheel(size);
+    };
+    var pi = Math.PI,
+        labelAttr = { stroke: "none", fill: "grey", "font-size": "12px" },
+        cursorAttr = { fill: "#222", stroke: "none", cursor: "move" };
+
+        ColorWheel = function (size) {
+            var t = this,
+                parts = 5,
+                w3 = 3.5 * size / 200,
+                size1 = size / 10,
+                size2 = size / 2.4,
+                center = size / 2,
+                segments = parts * 15,
+                padding = size1 * 1.8;
+
+            var r = Raphael(0, 0, size, size);
+            t.center = center;
+            var a = pi / 2 - pi * 2 / segments * 1.3 / 2,
+                R1 = center - padding,
+                R2 = R1 - size1 * 2,
+                path = ["M", center, padding, "A", R1, R1, 0, 0, 1, R1 * Math.cos(a) + R1 + padding, R1 - R1 * Math.sin(a) + padding, "L", R2 * Math.cos(a) + R1 + padding, R1 - R2 * Math.sin(a) + padding, "A", R2, R2, 0, 0, 0, center, padding + size1 * 2, "z"].join();
+            
+            for (var i = 0; i < segments; i++) {
+                var fill = i * (150 / segments) / 255;
+                if (i > segments / 2) {
+                    fill = (segments - i) * (150 / segments) / 255;
+                }
+                r.path(path).attr({
+                    stroke: "none",
+                    fill: Raphael.hsb(fill, .9, .78),
+                    transform: "r" + [(180 / segments) * i - 90, center, center]
+                });
+            }
+            t.cur = r.path(["M", center, size2, "L", center + w3 * 3, size2 - w3 * 4, "L", center, size2 - w3 * 12, "L", center - w3 * 3, size2 - w3 * 4]).attr(cursorAttr);
+            t.cur.drag(function (dx, dy, x, y) {
+                t.setH(x - center, y - center);
+            });
+            r.text(center, 20, "UNIT COUNTER").attr(labelAttr).attr({"font-size": "14px", "font-weight": "bold"});
+            r.text(center, 40,  "0").attr(labelAttr).attr({transform: "r" + [-90 * 5 / parts,   center, center] });
+            r.text(center, 40, "36").attr(labelAttr).attr({transform: "r" + [-90 * 3 / parts,   center, center] });
+            r.text(center, 40, "41").attr(labelAttr).attr({transform: "r" + [-90 * 1 / parts,   center, center] });
+            r.text(center, 40, "49").attr(labelAttr).attr({transform: "r" + [ 90 * 1 / parts,   center, center] });
+            r.text(center, 40, "51").attr(labelAttr).attr({transform: "r" + [ 90 * 3 / parts,   center, center] }); 
+            r.text(center, 40, "63").attr(labelAttr).attr({transform: "r" + [ 90 * 5 / parts,   center, center] });
+            t.lab = r.text(center, 180, "Drag the needle").attr(labelAttr);
+            r.text(20, 260, " 0-35: You need at least 36 units\n36-40: You may not be able to drop a course\n41-48: Healthy load\n49-50: You are piling too much on your plate\n51-63: requires permission if QPA < 3.0").attr(labelAttr).attr({"text-anchor": "start"});
+        },
+        proto = ColorWheel.prototype;
+    proto.setH = function (x, y) {
+        var labels = ["Underload", "Risky", "Healthy", "Overload", "Permissions"];
+        var d = Raphael.angle(x, y, 0, 0);
+        if (d < 90) { d = 360 } else if (d < 180) { d = 180 };
+        var i = Math.floor((d - 180) / 36);
+        if (i > 4) { i = 4 };
+        this.cur.attr({transform: "r" + [d + 90, this.center, this.center]});
+        this.lab.attr({text: labels[i]});
+    };
+})(window.Raphael);
--- static/js/counter.js
+++ static/js/counter.js
@@ -7,10 +7,10 @@ function Counter() {
 		sw = 12, 	// stroke-width
 		parts = 5, 	// number of stats
 		labelAttr = { stroke: "none", fill: "grey", "font-size": "12px" },
-		circleAttr = { fill: "#555", stroke: "none" };
+		circleAttr = { fill: "#555", stroke: "none" },
+		cursorAttr = { fill: "#222", stroke: "none", cursor: "move" };
 	
 	addObj();
-	legend("counter-legend", 250);
 
 	function addObj() {
 		var p = 80,
@@ -73,62 +73,6 @@ function Counter() {
 			}
 		});
 	}
-
-	function legend(el, size) {
-		var pi = Math.PI,
-        	labelAttr = { stroke: "none", fill: "grey", "font-size": "12px" },
-		    cursorAttr = { fill: "#222", stroke: "none", cursor: "move" },
-            parts = 5,
-            w3 = 3.5 * size / 200,
-            size20 = size / 10,
-            size2 = size / 2,
-            size4 = size / 2.4,
-            segments = parts * 15,
-            padding = size20 * 1.8,
-            t = this;
-		var r = Raphael(el, size, size * 2);
-		t.size2 = size2;
-		var a = pi / 2 - pi * 2 / segments * 1.3 / 2,
-		    R = size2 - padding,
-		    R2 = R - size20 * 2,
-		    path = ["M", size2, padding, "A", R, R, 0, 0, 1, R * Math.cos(a) + R + padding, R - R * Math.sin(a) + padding, "L", R2 * Math.cos(a) + R + padding, R - R2 * Math.sin(a) + padding, "A", R2, R2, 0, 0, 0, size2, padding + size20 * 2, "z"].join();
-		for (var i = 0; i < segments; i++) {
-		    var fill = i * (150 / segments) / 255;
-		    if (i > segments / 2) {
-		        fill = (segments - i) * (150 / segments) / 255;
-		    }
-		    r.path(path).attr({
-		        stroke: "none",
-		        fill: Raphael.hsb(fill, .9, .78),
-		        transform: "r" + [(180 / segments) * i - 90, size2, size2]
-		    });
-		}
-		t.cur = r.path(["M", size2, size4, "L", size2 + w3 * 3, size4 - w3 * 4, "L", size2, size4 - w3 * 12, "L", size2 - w3 * 3, size4 - w3 * 4]).attr(cursorAttr);
-		t.cur.drag(function (dx, dy, x, y) {
-		    setH(x - t.size2 - 10, y - t.size2 - 150, t);
-		});
-		var pad = window.screenY;
-		r.text(size2, 5 - pad, "UNIT COUNTER").attr(labelAttr).attr({"font-size": "14px", "font-weight": "bold"});
-		r.text(size2, size20 - 5 - pad, "0").attr(labelAttr).attr({transform: "r" + [-90 * 5 / parts,   size2, size2] });
-		r.text(size2, size20 - 5 - pad, "36").attr(labelAttr).attr({transform: "r" + [-90 * 3 / parts,   size2, size2] });
-		r.text(size2, size20 - 5 - pad, "41").attr(labelAttr).attr({transform: "r" + [-90 / parts,    size2, size2] });
-		r.text(size2, size20 - 5 - pad, "49").attr(labelAttr).attr({transform: "r" + [90 / parts,     size2, size2] });
-		r.text(size2, size20 - 5 - pad, "51").attr(labelAttr).attr({transform: "r" + [90 * 3 / parts,    size2, size2] });
-		r.text(size2, size20 - 5 - pad, "63").attr(labelAttr).attr({transform: "r" + [90 * 5 / parts,   size2, size2] });
-		t.leg = r.text(size2, 80 - pad, "Drag the needle").attr(labelAttr);
-		r.text(10, 100, " 0-35: You need at least 36 units\n36-40: You may not be able to drop a course\n41-48: Healthy load\n49-50: You are piling too much on your plate\n51-63: requires permission if QPA < 3.0").attr(labelAttr).attr({"text-anchor": "start"});
-	}
-
-	function setH(x, y, t) {
-		var labels = ["Underload", "Risky", "Healthy", "Overload", "Permissions"];
-        var d = Raphael.angle(x, y, 0, 0);
-        if (d < 90) { d = 360 }
-        else if (d < 180) { d = 180 };
-        var i = Math.floor((d - 180) / 36);
-        if (i > 4) { i = 4 };
-        t.cur.attr({transform: "r" + [d + 90, this.size2, this.size2]});
-        t.leg.attr({text: labels[i], y: 155});
-    }
 
 	this.updateVal = function (i, v) {
 		var value;
