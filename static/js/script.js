63d7bd1ec582914e58c4ff59b0e3cee6e18187d7
Kanbour Sama <samak@cmu.edu>
2015-04-07 14:20:18 +0300

    .

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,5 @@
 var USER = null;
 var VIEWER = false;
-var opts = {
-  lines: 13, // The number of lines to draw
-  length: 20, // The length of each line
-  width: 10, // The line thickness
-  radius: 30, // The radius of the inner circle
-  corners: 1, // Corner roundness (0..1)
-  rotate: 0, // The rotation offset
-  direction: 1, // 1: clockwise, -1: counterclockwise
-  color: '#555', // #rgb or #rrggbb or array of colors
-  speed: 1, // Rounds per second
-  trail: 60, // Afterglow percentage
-  shadow: false, // Whether to render a shadow
-  hwaccel: false, // Whether to use hardware acceleration
-  className: 'spinner', // The CSS class to assign to the spinner
-  zIndex: 2e9, // The z-index (defaults to 2000000000)
-  top: '50%', // Top position relative to parent
-  left: '50%' // Left position relative to parent
-};
-var target = document.getElementById('spinner');
-var spinner = new Spinner(opts).spin(target);
 
 document.addEventListener('DOMContentLoaded', function () {
 	setUp();
@@ -248,6 +228,26 @@ function addAdvisor(id, name){
 }
 
 function setUp(){
+	var opts = {
+		lines: 13, // The number of lines to draw
+		length: 20, // The length of each line
+		width: 10, // The line thickness
+		radius: 30, // The radius of the inner circle
+		corners: 1, // Corner roundness (0..1)
+		rotate: 0, // The rotation offset
+		direction: 1, // 1: clockwise, -1: counterclockwise
+		color: '#555', // #rgb or #rrggbb or array of colors
+		speed: 1, // Rounds per second
+		trail: 60, // Afterglow percentage
+		shadow: false, // Whether to render a shadow
+		hwaccel: false, // Whether to use hardware acceleration
+		className: 'spinner', // The CSS class to assign to the spinner
+		zIndex: 2e9, // The z-index (defaults to 2000000000)
+		top: '50%', // Top position relative to parent
+		left: '50%' // Left position relative to parent
+	};
+	var target = document.getElementById('spinner');
+	var spinner = new Spinner(opts).spin(target);
 	$("#log div").click(function () {
 		if (this.innerHTML == "LOGIN") {
 			window.location.href = 'private';
